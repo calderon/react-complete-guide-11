f7a52c5e8302fc724b8553a3a28af6823937f8f1
Daniel Calderon <daniel.calderon.portillo@gmail.com>
2023-06-12 11:45:16 +0200

    Cart improved

--- src/Cart/index.js
+++ src/Cart/index.js
@@ -1,13 +1,18 @@
+import { Fragment } from "react";
+
 import CartIcon from "./CartIcon.js";
+
 import classes from './Cart.module.css';
 
 const Cart = () => {
   return (
-    <button className={classes.cart}>
-      <CartIcon className={classes["cart__icon"]} />
-      <span>Your cart</span>
-      <span className={classes["cart__badge"]}>0</span>
-    </button>
+    <Fragment>
+      <button className={classes.cart}>
+        <CartIcon className={classes["cart__icon"]} />
+        <span>Your cart</span>
+        <span className={classes["cart__badge"]}>0</span>
+      </button>
+    </Fragment>
   )
 };
 
